6aa0f292e3df6e2dce0f69cb8744ad0a1f807f9c
Dan Ackers <dakerlun@student.oulu.fi>
2017-10-09 20:54:33 +0300

    Added janitor rotation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var updates = [];
 var nextUpdate = {amount: 0, action: ()=>{}};
 
-var janitorDefaultPos = {x: 50, y: 450};
+var janitorDefaultPos = {x: 70, y: 480};
 
 function init(){
 	//CODE MIRROR
@@ -24,11 +24,15 @@ function init(){
 	var stage = new createjs.Stage("codeCanvas");
 	var pohja = new createjs.Bitmap("./pohjapiirros.png");
 	stage.addChild(pohja);
-
-	var sotku = new createjs.Bitmap("./dirt.png");
+	console.log(stage);
+	console.log(pohja);
+	var sotku = new createjs.Bitmap("./dirt2.png");
 	stage.addChild(sotku);
 
+
 	var janitorsprite = new createjs.Bitmap("./janitor.png");
+	janitorsprite.regX=32;
+	janitorsprite.regY=32;
 	janitorsprite.x = janitorDefaultPos.x;
 	janitorsprite.y = janitorDefaultPos.y;
 	Janitor.sprite = janitorsprite;
@@ -57,5 +61,7 @@ var Janitor = {
 	forward: (amt)=>{updates.push({amount: amt || 50, action: ()=>{Janitor.sprite.y--;}})},
 	left: (amt)=>{updates.push({amount: amt || 90, action: ()=>{Janitor.sprite.x--;}})},
 	right: (amt)=>{updates.push({amount: amt || 90, action: ()=>{Janitor.sprite.x++;}})},
-	backwards: (amt)=>{updates.push({amount: amt || 50, action: ()=>{Janitor.sprite.y++;}})}
+	backwards: (amt)=>{updates.push({amount: amt || 50, action: ()=>{Janitor.sprite.y++;}})},
+	turnRight: (amt) =>{updates.push({amount:amt || 90, action: ()=>{Janitor.sprite.rotation++;}})},
+	turnLeft: (amt) =>{updates.push({amount:amt || 90, action: ()=>{Janitor.sprite.rotation--;}})}
 }
